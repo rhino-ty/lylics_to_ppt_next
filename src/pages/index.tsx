75639e78e7eb73aa317070c3fbc4ebdf4d7ad43d
Rhino-ty <okko8522@gmail.com>
2023-04-21 00:58:04 +0900

    feat: 기본 코드 지우고 pptxgenjs를 사용한 ppt 생성 코드 구현
    form으로 제출식, handleFormSubmit함수로 제출하여 \n를 기준으로 나눠 배열로 만든 후 ppt 생성

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,124 +1,74 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Head from "next/head";
+import pptxgen from "pptxgenjs";
 
-const inter = Inter({ subsets: ['latin'] })
+type SlideProps = {
+  pptx: pptxgen;
+  lines: string[];
+  startIndex: number;
+  endIndex: number;
+  fontSize: number;
+  maxLines: number;
+};
 
 export default function Home() {
+  const addSlide = ({ pptx, lines, startIndex, endIndex, fontSize, maxLines }: SlideProps) => {
+    const slide = pptx.addSlide();
+    const text = lines.slice(startIndex, endIndex).join("\n");
+    slide.background = { color: "000000" };
+    slide.color = "ffffff";
+    slide.addText([{ text: text, options: { fontSize: fontSize, align: "center" } }], {
+      x: 0.5,
+      y: 0.5,
+      w: 9,
+      h: 6.5,
+    });
+  };
+
+  const handleFormSubmit = (event: any) => {
+    event.preventDefault();
+    const lyrics = event.target.lyrics.value;
+    const fontSize = event.target.fontSize.value || 50;
+    const maxLines = event.target.maxLines.value || 4;
+    const lines = lyrics.split("\n");
+    const pptx = new pptxgen();
+    pptx.layout = "LAYOUT_4x3";
+    pptx.theme = { bodyFontFace: "Nanum Gothic" };
+    for (let i = 0; i < lines.length; i += maxLines) {
+      addSlide({
+        pptx,
+        lines,
+        startIndex: i,
+        endIndex: i + maxLines,
+        fontSize: fontSize,
+        maxLines: maxLines,
+      });
+    }
+    pptx.writeFile(lines[0]);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div>
+      <Head>
+        <title>가사 PPT</title>
+      </Head>
+
+      <div className="container">
+        <h1>PPT로 만들어보세요!</h1>
+        <p>가사를 엔터공백 없이 입력하면 입력한 줄 수마다 PPT 섹션으로 만들어져요!</p>
+        <p>첫 줄은 파일 이름으로 나옵니다!</p>
+        <form onSubmit={handleFormSubmit}>
+          <textarea name="lyrics" rows={10}></textarea>
+          <div>
+            <label htmlFor="fontSize">폰트 사이즈:</label>
+            <input type="number" name="fontSize" defaultValue="50" />
+          </div>
+          <div>
+            <label htmlFor="maxLines">PPT 한 섹션에 들어갈 가사 줄 수:</label>
+            <input type="number" name="maxLines" defaultValue="4" />
+          </div>
+          <button type="submit">PPT 생성</button>
+        </form>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+  );
 }
