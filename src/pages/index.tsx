943fd11ed4a0e422bf275a08703c2f40eb725c0e
Rhino-ty <okko8522@gmail.com>
2023-04-21 01:10:28 +0900

    feat: 옵션 변수들을 지역 상태로 관리

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import pptxgen from "pptxgenjs";
+import { useState } from "react";
 
 type SlideProps = {
   pptx: pptxgen;
@@ -11,6 +12,9 @@ type SlideProps = {
 };
 
 export default function Home() {
+  const [fontSize, setFontSize] = useState<number>(50);
+  const [maxLines, setMaxLines] = useState<number>(4);
+
   const addSlide = ({ pptx, lines, startIndex, endIndex, fontSize, maxLines }: SlideProps) => {
     const slide = pptx.addSlide();
     const text = lines.slice(startIndex, endIndex).join("\n");
@@ -27,8 +31,10 @@ export default function Home() {
   const handleFormSubmit = (event: any) => {
     event.preventDefault();
     const lyrics = event.target.lyrics.value;
-    const fontSize = event.target.fontSize.value || 50;
-    const maxLines = event.target.maxLines.value || 4;
+    if (!lyrics) {
+      alert("가사를 입력해주세요!");
+      return;
+    }
     const lines = lyrics.split("\n");
     const pptx = new pptxgen();
     pptx.layout = "LAYOUT_4x3";
@@ -54,18 +60,32 @@ export default function Home() {
 
       <div className="container">
         <h1>PPT로 만들어보세요!</h1>
-        <p>가사를 엔터공백 없이 입력하면 입력한 줄 수마다 PPT 섹션으로 만들어져요!</p>
-        <p>첫 줄은 파일 이름으로 나옵니다!</p>
+        <p>가사를 입력하면 입력한 줄 수마다 PPT 페이지로 만들어져요.</p>
         <form onSubmit={handleFormSubmit}>
+          <span>첫 줄은 파일 이름으로 나옵니다!</span>
           <textarea name="lyrics" rows={10}></textarea>
-          <div>
-            <label htmlFor="fontSize">폰트 사이즈:</label>
-            <input type="number" name="fontSize" defaultValue="50" />
+          <div className="option-container">
+            <div>
+              <label htmlFor="fontSize">폰트 사이즈 :</label>
+              <input
+                type="number"
+                name="fontSize"
+                value={fontSize}
+                onChange={(e) => setFontSize(Number(e.target.value))}
+              />
+            </div>
+            <div>
+              <label htmlFor="maxLines">한 페이지에 들어갈 가사 줄 수 : </label>
+              <input
+                type="number"
+                name="maxLines"
+                value={maxLines}
+                onChange={(e) => setMaxLines(Number(e.target.value))}
+              />
+            </div>
           </div>
-          <div>
-            <label htmlFor="maxLines">PPT 한 섹션에 들어갈 가사 줄 수:</label>
-            <input type="number" name="maxLines" defaultValue="4" />
-          </div>
+          <div className="mt-3">가사 한줄 한줄이 너무 길다 싶으면 임의로 줄바꿈을 해주세요!</div>
+          <br />
           <button type="submit">PPT 생성</button>
         </form>
       </div>
